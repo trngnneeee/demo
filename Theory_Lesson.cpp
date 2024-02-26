5a37416eb985aab3e6f4d3e75e92e7c7caa3ce00
trngnneee <dtnguyen23@clc.fitus.edu.vn>
2024-02-26 08:12:55 +0700

    fixed Ex01

--- Theory_Lesson.cpp
+++ Theory_Lesson.cpp
@@ -15,12 +15,23 @@ node* makeNode(int x){
 
 node* findX(node* head, int x){
     node* tmp = head;
-    if (head == NULL) return NULL;
+    //if (head == NULL) return NULL; // we dont need this line because this was already checked
     while (tmp != NULL && tmp -> data <= x)
     {
         if (tmp -> data == x) return tmp;
-        tmp = tmp -> next;
+        tmp = tmp -> next;  
     }
+    return NULL;
+}
+
+node* findXFixed(node* head, int x){
+    node* tmp = head;
+    //if (head == NULL) return NULL; // we dont need this line because this was already checked
+    while (tmp != NULL && tmp -> data < x)
+    {
+        tmp = tmp -> next;  
+    }
+    if (tmp && tmp -> data == x) return tmp;    // check if tmp is a null to avoid accessing to null data
     return NULL;
 }
 
